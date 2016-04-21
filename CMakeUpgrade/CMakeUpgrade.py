42288dc447000b11b01fb152e1dbc2c7f671fbc1
sassi67 <sassi67@libero.it>
2016-04-21 21:31:54 +0200

    xzvcxzv

--- CMakeUpgrade/CMakeUpgrade.py
+++ CMakeUpgrade/CMakeUpgrade.py
@@ -2,16 +2,38 @@ from sys import argv, exit
 from Upgrade import Upgrade
 import os 
 
-if len(argv) < 3:
-    exit(1)
+import abc
+class Interface():
+    """Base class for common operations"""
+    __metaclass__ = abc.ABCMeta
 
-x, rootDir, cMakeFile = argv
+    @abc.abstractmethod
+    def f(self):
+         pass
 
-for dirpath, dirs, files in os.walk(rootDir):	 
-	path = dirpath.split('/')
-	print '|', (len(path))*'---', '[',os.path.basename(dirpath),']'
-	for f in files:
-		print '|', len(path)*'---', f
+class Impl(Interface):
+    def __init__(self, *args, **kwargs):
+        return super(Impl, self).__init__(*args, **kwargs)
+    def f(self):
+        print "Im am an implementation"
+        return super(Impl, self).f() 
+    def g(self):
+        print "I'm g"
+         
+obj = Impl()
+obj.g()
+
+#if len(argv) < 3:
+#    exit(1)
+
+#x, rootDir, cMakeFile = argv
+
+#for dirpath, dirs, files in os.walk(rootDir):	 
+#	path = dirpath.split('/')
+#	print '|', (len(path))*'---', '[',os.path.basename(dirpath),']'
+#	for f in files:
+#		print '|', len(path)*'---', f
+
 #fileList = []
 #for root, dirs, files in os.walk(root_directory):
 #    for dir in dirs:
